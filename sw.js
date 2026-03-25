74106b2b2c543ce241a33e82b24672bb29be10d7
sistematikos <f4anks@gmail.com>
2026-03-24 22:49:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,21 @@
-const CACHE_NAME = 'data-ciar-v1';
-const assets = [
-  'index.html',
-  'index_listaciar.html',
-  'index_mdfciar.html',
-  'js/ciar_app.js',
-  'manifest.json'
+const CACHE_NAME = 'ciar-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index_listaciar.html',
+  '/js/ciar_app.js',
+  '/manifest.json',
+  '/icon-192.png'
 ];
 
-// Instalar Service Worker
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activar y limpiar caches viejos
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// Responder desde cache o red
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
