2296a4d0505dcf17cbab3c6e19c2edd17602e253
sistematikos <f4anks@gmail.com>
2026-03-24 22:46:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = 'avel-v2';
-const urlsToCache = [
+const CACHE_NAME = 'data-ciar-v1';
+const assets = [
   'index.html',
   'index_listaciar.html',
-  'index_ciar.html',
-  'js/ciar_app.js'
+  'index_mdfciar.html',
+  'js/ciar_app.js',
+  'manifest.json'
 ];
 
+// Instalar Service Worker
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activar y limpiar caches viejos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// Responder desde cache o red
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
